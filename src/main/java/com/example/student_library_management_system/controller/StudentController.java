6302a98379a835f91162d9b0601de31e8b8438d7
ahamadarbab <ahmadarbab.456@gmail.com>
2026-01-26 20:47:38 +0530

    Created Student Controller and Service class + performed CRUD operations with APIs

--- src/main/java/com/example/student_library_management_system/controller/StudentController.java
+++ src/main/java/com/example/student_library_management_system/controller/StudentController.java
@@ -1,4 +1,54 @@
 package com.example.student_library_management_system.controller;
 
+import com.example.student_library_management_system.model.Student;
+import com.example.student_library_management_system.requestdto.StudentRequestDto;
+import com.example.student_library_management_system.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student/apis")
 public class StudentController {
+
+    @Autowired
+    StudentService studentService;
+
+    @PostMapping("/save")
+    public String saveStudent(@RequestBody StudentRequestDto studentRequestDto) {
+        String response = studentService.saveStudent(studentRequestDto);
+        return response;
+    }
+
+    @GetMapping("/findById/{id}")
+    public Student findStudentById(@PathVariable int id) {
+        Student student = studentService.findStudentById(id);
+        return student;
+    }
+
+    @GetMapping("/findAll")
+    public List<Student> findAllStudent() {
+        List<Student> studentList = studentService.findAllStudents();
+        return studentList;
+    }
+
+    @GetMapping("/count")
+    public String countStudent() {
+        String response = studentService.countStudent();
+        return response;
+    }
+
+    @DeleteMapping("/deleteById/{id}")
+    public String deleteStudentById(@PathVariable int id) {
+        String response = studentService.deleteStudentById(id);
+        return response;
+    }
+
+    @PutMapping("/updateByPut/{id}")
+    public String updateStudentUsingPut(@PathVariable int id, @RequestBody StudentRequestDto studentRequestDto) {
+        String response = studentService.updateStudentUsingPut(id, studentRequestDto);
+        return response;
+    }
+
 }
--- src/main/java/com/example/student_library_management_system/service/StudentService.java
+++ src/main/java/com/example/student_library_management_system/service/StudentService.java
@@ -1,4 +1,106 @@
 package com.example.student_library_management_system.service;
 
+import com.example.student_library_management_system.enums.CardStatus;
+import com.example.student_library_management_system.model.Card;
+import com.example.student_library_management_system.model.Student;
+import com.example.student_library_management_system.repository.StudentRepository;
+import com.example.student_library_management_system.requestdto.StudentRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class StudentService {
+
+    @Autowired
+    StudentRepository studentRepository;
+
+    public String saveStudent(StudentRequestDto studentRequestDto) {
+
+        // convert the request DTO into model class
+        Student student = new Student();
+        student.setStudentName(studentRequestDto.getStudentName());
+        student.setEmail(studentRequestDto.getEmail());
+        student.setPhone(studentRequestDto.getPhone());
+        student.setDob(studentRequestDto.getDob());
+        student.setGender(studentRequestDto.getGender());
+        student.setAddress(studentRequestDto.getAddress());
+        student.setSemester(studentRequestDto.getSemester());
+        student.setCourse(studentRequestDto.getCourse());
+        student.setDept(studentRequestDto.getDept());
+
+        // whenever student adds, card also gets added as part of cascading
+        Card card = new Card();
+        card.setCardStatus(CardStatus.ACTIVE);
+        card.setExpiryDate(LocalDate.now().plusYears(3).toString());
+        card.setMaxBooksAllowed(5);
+        card.setStudent(student);
+        student.setCard(card);
+
+        studentRepository.save(student);
+        return "Student saved successfully!";
+    }
+
+    public Student findStudentById(int id) {
+        Optional<Student> studentOptional = studentRepository.findById(id);
+        if(studentOptional.isPresent())
+        {
+            return studentOptional.get();
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public List<Student> findAllStudents() {
+        List<Student> studentList = studentRepository.findAll();
+        return studentList;
+    }
+
+    public String countStudent() {
+       long totalStudentCount = studentRepository.count();
+       return "Total number of students are: " + totalStudentCount;
+
+    }
+
+    public String deleteStudentById(int id) {
+        Student student = findStudentById(id);
+        if(student != null)
+        {
+            studentRepository.deleteById(id);
+            return "Student with id: " + id + " got deleted successfully!";
+        }
+        else
+        {
+            return "Student with id: " + id + " is not present, hence cannot delete";
+        }
+    }
+
+    public String updateStudentUsingPut(int id, StudentRequestDto newStudentRequestDto) {
+        Student existingStudent = findStudentById(id);
+        if(existingStudent != null)
+        {
+            existingStudent.setStudentName(newStudentRequestDto.getStudentName());
+            existingStudent.setEmail(newStudentRequestDto.getEmail());
+            existingStudent.setPhone(newStudentRequestDto.getPhone());
+            existingStudent.setDob(newStudentRequestDto.getDob());
+            existingStudent.setGender(newStudentRequestDto.getGender());
+            existingStudent.setAddress(newStudentRequestDto.getAddress());
+            existingStudent.setSemester(newStudentRequestDto.getSemester());
+            existingStudent.setCourse(newStudentRequestDto.getCourse());
+            existingStudent.setDept(newStudentRequestDto.getDept());
+
+            studentRepository.save(existingStudent);
+            return "Student with id: " + id + " got updated successfully!";
+        }
+        else
+        {
+            return "Student with id: " + id + " is not present, hence cannot update";
+        }
+    }
+
 }
